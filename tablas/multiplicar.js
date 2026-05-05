c29be48ab8ac1e72339f13e14343dbb78d812d13
JHOON231 <jhondavis.quinterot@gmail.com>
2026-05-04 22:56:22 -0500

    Instrucciones y mensajes intitivos agregados

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,9 +1,26 @@
 function generarTablas(){
     let contenedor = document.getElementById("contenedor");
-    let contenido = "";
+    let numero = parseInt(document.getElementById("txtNumero").value);
+
+    if (isNaN(numero)) {
+        contenedor.innerHTML = "<p style='color: yellow;'> Escribe un número primero </p>";
+        return;
+    }
+
+    let contenido = `<h2> Tabla del ${numero} </h2>`;
+
     for(let i =0; i <= 10; i++){
-        contenido += "<div class='fila'>5 x " + i + " = " + (5 * i) + "</div>";
+        contenido += "<div class='fila'> " + numero + " x " + i + " = " + (numero * i) + "</div>";
     }
 
     contenedor.innerHTML = contenido;
+}
+
+function volver() {
+    let contenedor = document.getElementById("contenedor");
+    let input = document.getElementById("txtNumero");
+
+    contenedor.innerHTML = ""; 
+    input.value = "";          
+    input.focus();             
 }
